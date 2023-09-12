2452d3d657ef1ca38761c4396dda42127394d8c6
ArkanZAG <arkanalghifari22@gmail.com>
2023-09-12 17:22:27 +0700

    Progress#1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float limitValue;
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MovePlayer();
+        }
+    }
+
+    private void MovePlayer()
+    {
+        float halfScreen = Screen.width / 2;
+        float xPosition = (Input.mousePosition.x - halfScreen) / halfScreen;
+        float finalXPosition = Mathf.Clamp(xPosition * limitValue, -limitValue, limitValue);
+        playerTransform.localPosition = new Vector3(finalXPosition, 1, 0);
+    }
+}
